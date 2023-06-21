de3f399cf6a806a24022015c6e272b5ed9c68c31
ToshikiKanai <50226151+ToshikiKanai@users.noreply.github.com>
2023-06-22 03:26:22 +0900

    Update ConnectBT.java
    for Raspberry Pi and works on any Device

--- app/src/main/java/com/example/shin_utsurundesu/ConnectBT.java
+++ app/src/main/java/com/example/shin_utsurundesu/ConnectBT.java
@@ -16,14 +16,15 @@ import java.util.UUID;
 
 public class ConnectBT extends AsyncTask<Void, Void, BluetoothSocket> {
     private static final String DEVICE_ADDRESS = "B8:27:EB:D7:92:D5";
-    private static final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");  // Raspberry Pi側も同じにしてね
+    // private static final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");  // Raspberry Pi側も同じにしてね
+    private static final UUID SERVICE_UUID = UUID.fromString("e288efca-8008-4105-99f0-072c8e9e51a3"); // 任意のUUID
     private static final String TAG = "CBT";
     private boolean isConnected = false;
 
     private Activity mParentActivity;
     private BluetoothAdapter mAdapter;
     private BluetoothSocket mSocket;
-    private BluetoothDevice mDevice;
+    // private BluetoothDevice mDevice;
     private ConnectBTCallback mCallback;
 
 
@@ -41,40 +42,74 @@ public class ConnectBT extends AsyncTask<Void, Void, BluetoothSocket> {
 
     @Override
     protected BluetoothSocket doInBackground(Void... params) {
-        if (ActivityCompat.checkSelfPermission(mParentActivity, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            return null;
+
+        if (Build.VERSION.SDK_INT >= 31) {
+            if (ActivityCompat.checkSelfPermission(mParentActivity, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                //todo  ActivityCompat.checkSelfPermission
+            }
+        }else{
+            if (ActivityCompat.checkSelfPermission(mParentActivity, android.Manifest.permission.BLUETOOTH)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(mParentActivity, new String[]{
+                        android.Manifest.permission.BLUETOOTH
+                }, REQUEST_CODE);
+            }
+            if (ActivityCompat.checkSelfPermission(mParentActivity, android.Manifest.permission.BLUETOOTH_ADMIN)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(mParentActivity, new String[]{
+                        android.Manifest.permission.BLUETOOTH_ADMIN
+                }, REQUEST_CODE);
+            }
         }
         if (!isConnected) {
-            mDevice = mAdapter.getRemoteDevice(DEVICE_ADDRESS);
-
             try {
-                mSocket = mDevice.createInsecureRfcommSocketToServiceRecord(PORT_UUID);
+                Set<BluetoothDevice> pairedDevices = this.mAdapter.getBondedDevices();
+                Log.d("App", String.valueOf(pairedDevices.size()));
+                if (pairedDevices.size() > 0) {
+                    /*
+                    事前にペアリング済みのデバイスを全て取得
+                    ペアリング対象の DEVICE_ADDRESS(MAC address) or deviceName をUIから選択できるようにすれば
+                    任意のデバイスとのペアリングができます
+                    */
+                    for (BluetoothDevice device : pairedDevices) {
+                        String deviceName = device.getName();
+                        String deviceHardwareAddress = device.getAddress(); // MAC address
+                        Log.d("App", deviceName);
+                        Log.d("App", deviceHardwareAddress);
+                        Log.d("App", DEVICE_ADDRESS);
+                        if (deviceHardwareAddress.equals(DEVICE_ADDRESS)) {
+                            Log.d("App", "Device Found! (" + DEVICE_ADDRESS + ")");
+                            mSocket = device.createRfcommSocketToServiceRecord(SERVICE_UUID);
+                            Log.d("App", "Got BluetoothSocket");
+                            if (mAdapter.isDiscovering()) {
+                                mAdapter.cancelDiscovery();
+                            }
+                            try {
+                                mSocket.connect();
+                                isConnected = true;
+                                Log.d("App", "Connected");
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                Log.d(TAG, "Unable to connect.");
+                                try {
+                                    mSocket.close();
+                                    Log.d(TAG, "Unable to close socket.");
+                                } catch (IOException closeException) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                        }
+                    }
+                }
             } catch (IOException e) {
                 e.printStackTrace();
                 return null;
             }
-
-            if (BluetoothAdapter.getDefaultAdapter().isDiscovering()) {
-                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
-            }
-
-            try {
-                mSocket.connect();
-                isConnected = true;
-            } catch (IOException e) {
-                e.printStackTrace();
-                Log.d(TAG, "Unable to connect.");
-                try {
-                    mSocket.close();
-                    Log.d(TAG, "Unable to close socket.");
-                } catch (IOException closeException) {
-                    e.printStackTrace();
-                }
-            }
         }
         return mSocket;
     }
-
+    
     @Override
     protected void onPostExecute(BluetoothSocket socket) {
         super.onPostExecute(socket);
